4261a35feee77d4406f9eeaefda4ff85afbb2a1c
root <root@mobile-ao.local>
2025-03-21 03:20:01 +0000

    fix app.css di file layouts/third.blade.php

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+		'resources/css/app.css',
                 'resources/css/main.css',
                 'resources/css/output.css',
                 'resources/js/app.js',
+		'resources/js/take.js',
             ],
             refresh: true,
         }),
