1db732539e287d3af21c6a9f72321d9870120e24
Galih Setiabudi <redplakad@gmail.com>
2025-03-26 04:18:57 +0700

    penambahan banyak aset di nominatif cabang, seperti filter dan tombol detail saat user klik tombol detail dari salah satu debitur

--- app/Http/Controllers/KreditController.php
+++ app/Http/Controllers/KreditController.php
@@ -12,24 +12,43 @@ class KreditController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $kredit = Kredit::paginate(10); // Ambil data dengan pagination (10 per halaman)
         $totalRow = Kredit::count(); // Hitung total semua row tanpa pagination
         
-        return view('nominatif.index', compact('user','kredit', 'totalRow'));
+        return view('nominatif.index', compact('user', 'totalRow'));
     }
     // app/Http/Controllers/NominatifController.php
 
-    public function showByBranch($branch_code)
+    public function showByBranch($branch_code, Request $request)
     {
         $user = Auth::user();
-        // Ambil data berdasarkan branch_code
-        $nominatifs = Kredit::where('CAB', $branch_code)->paginate(15);
+        $datadate = "2025-03-24";
+        // Ambil daftar unik dari AO, Produk, dan Instansi
+        $listAO = Kredit::where("datadate", $datadate)->where('CAB',$user->branch?->branch_code)->select('AO')->distinct()->pluck('AO');
+        $listProduk = Kredit::where("datadate", $datadate)->where('CAB',$user->branch?->branch_code)->select('KET_KD_PRD')->distinct()->pluck('KET_KD_PRD');
+        $listInstansi = Kredit::where("datadate", $datadate)->where('CAB',$user->branch?->branch_code)->select('TEMPAT_BEKERJA')->distinct()->pluck('TEMPAT_BEKERJA');
 
-        // Hitung total debitur
-        $totalRow = $nominatifs->count();
+        $query = Kredit::where('datadate', $datadate);
+
+        if ($request->filled('kolektibilitas')) {
+            $query->where('KODE_KOLEK', $request->kolektibilitas);
+        }
+    
+        if ($request->filled('ao')) {
+            $query->where('AO', $request->ao);
+        }
+    
+        if ($request->filled('produk')) {
+            $query->where('KET_KD_PRD', $request->produk);
+        }
+    
+        if ($request->filled('instansi')) {
+            $query->where('TEMPAT_BEKERJA', $request->instansi);
+        }
+    
+        $kredit = $query->paginate(10);
 
         // Kirim ke view
-        return view('nominatif.branch', compact('user','nominatifs', 'branch_code', 'totalRow'));
+        return view('nominatif.branch', compact('user','kredit', 'listAO', 'listProduk', 'listInstansi'));
     }
 
 }
